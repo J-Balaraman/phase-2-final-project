799cfc246d93c2c7ad06988962e808be9bbde38f
jbalaraman <james.balaraman@gmail.com>
2023-10-25 15:50:24 -0600

    Added data/functionality to the ExtraSongInfo componenent, and modified a key name in the db.json file

--- src/ExtraSongInfo.js
+++ src/ExtraSongInfo.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "./NavBar";
 
-function ExtraSongInfo(){
+function ExtraSongInfo() {
+    const [extraSongData, setExtraSongData] = useState([]);
+    const { title } = useParams();
+
+    useEffect(() => {
+        fetch('http://localhost:6001/songs')
+            .then((res) => res.json())
+            .then((data) => {
+                setExtraSongData(data);
+            })
+    }, []);
+
+    const extraData = extraSongData.find((song) => song.title === title);
+
     return (
-        <div>
-            Extra Song Info Here
-        </div>
-    )
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <div className="song-info">
+                    <li>{`Song: ${extraData && extraData.title}`}</li>
+                    <li>{`Artist: ${extraData && extraData.artist}`}</li>
+                    <li>{`Album: ${extraData && extraData.album}`}</li>
+                    <li>{`Genre: ${extraData && extraData.genre}`}</li>
+                    <li>{`Release Date: ${extraData && extraData.releaseDate}`}</li>
+                    <li>{`Duration: ${extraData && extraData.duration}`}</li>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default ExtraSongInfo;
