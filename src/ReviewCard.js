b2b4bd12568d78ddefe71ca58269238d7126e215
jbalaraman <james.balaraman@gmail.com>
2023-10-26 15:10:55 -0600

    Created reviews.css and imported into reviews.js

--- src/ReviewCard.js
+++ src/ReviewCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./ReviewForm.css"
+import "./ReviewCard.css"
 
 function ReviewCard({reviews}) {
     return(
--- src/Reviews.js
+++ src/Reviews.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import ReviewCard from "./ReviewCard";
 import ReviewForm from "./ReviewForm";
+import "./Reviews.css"
 
 function Reviews() {
     const [reviews, setReviews] = useState([]);
