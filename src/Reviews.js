74ab1627d1a23f0f47fc5fbf68f952c1996b382d
jbalaraman <james.balaraman@gmail.com>
2023-10-26 14:12:50 -0600

    Added navbar component to the reviews component

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,8 +1,12 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 function Reviews() {
     return(
         <>
+        <header>
+            <NavBar/>
+        </header>
         Beans
         </>
     )
