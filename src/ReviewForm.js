421a14097d23a7beb668fb6c7799eb532f4ebe85
jbalaraman <james.balaraman@gmail.com>
2023-10-26 14:54:47 -0600

    Added post request to simulated backend, data submission and its display

--- src/ReviewForm.js
+++ src/ReviewForm.js
@@ -1,11 +1,53 @@
 import React, { useState, useEffect } from "react";
 
-function ReviewForm() {
+function ReviewForm({onAddReview}) {
+    const [reviewContent, setReviewContent] = useState('')
+    const [user, setUser] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const reviewData = {
+            user: user,
+            reviewContent: reviewContent
+        };
+
+        fetch("http://localhost:6001/reviews", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(reviewData),
+        })
+            .then((r) => r.json())
+            .then((newReview) => {
+                setReviewContent("");
+                setUser("");
+                onAddReview(newReview)
+            })
+    }
+
     return(
         <main>
-            <>
-                Bingus
-            </>
+            <h2>Leave A Review</h2>
+            <div className="new-review-form">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="reviewContent"
+                        placeholder="Let us know what you think!"
+                        value={reviewContent}
+                        onChange={(e) => setReviewContent(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        name="user"
+                        placeholder="What's your name?"
+                        value={user}
+                        onChange={(e) => setUser(e.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </main>
     )
 }
